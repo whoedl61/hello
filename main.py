f6b76bf044b6853a05247226808c6316b79cd915
whoedl61 <wo4101@yahoo.de>
2022-01-07 23:39:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime
+from datetime import date
+
+
+def print_hi(name,date):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}, nuts , {date}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm' , date=datetime.datetime.now())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
